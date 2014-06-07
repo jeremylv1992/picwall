0890a27aee6a704ddfdd4530f36ed378668e6ca6
doublehh <sserdoubleh@gmail.com>
2014-06-07 11:02:32 +0800

    1st iter is over.

--- picwall/admin.py
+++ picwall/admin.py
@@ -1,12 +1,3 @@
-#########################################################################
-# File Name: admin.py
-# Author: Jeremy
-# mail: Jeremy19921115@gmail.com
-# Created Time: Sat 31 May 2014 08:34:35 PM CST
-#########################################################################
-#!/usr/bin/env python
-#coding=utf-8
-
 from django.contrib import admin
 from picwall.models import pw_pic, pic_comment, PhotoWall, PhotoInformation
 
