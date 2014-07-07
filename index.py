0774090404bd723754fb3f34bc98ddea36f06651
doublehh <sserdoubleh@gamil.com>
2014-07-07 10:58:26 +0800

    a

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ import sys
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+path = os.path.dirname(os.path.abspath(__file__)) + '/mysite'
+if path not in sys.path:
+    sys.path.insert(1, path)
 
 from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
