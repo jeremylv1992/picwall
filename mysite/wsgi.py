8ae5d85b785dab91e498bacfdc37ce3ce0bea480
doublehh <doublehh@ubuntu.(none)>
2014-07-07 09:24:45 +0800

    test baidu yun again

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,12 @@
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
+path = os.path.dirname(os.path.abspath(__file__)) + '/mysite'
+if path not in sys.path:
+	sys.path.insert(1, path)
+
+from bae.core.wsgi import WSGIApplication
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
