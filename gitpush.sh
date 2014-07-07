3c41e130410f681248018c6ca2a7ca31727010bf
doublehh <sserdoubleh@gamil.com>
2014-07-07 10:41:50 +0800

    a

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,3 @@
 git add *
 git commit -m "a"
 git push origin master
-HiukinChu
-zhaopianqiang
--- index.py
+++ index.py
@@ -1,4 +1,9 @@
 #-*- coding:utf-8 -*-
+import sys
+import os
+
+os.environ.setdefault(['DJANGO_SETTINGS_MODULE'], 'mysite.settings')
+
 from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
 		 
