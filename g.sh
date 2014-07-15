b6083908c19cdf8165a62da7dcabd20265f1f47b
doublehh <sserdoubleh@gamil.com>
2014-07-15 13:45:13 +0800

    tp

---
+++ g.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "tp"
+git push -f origin master
--- picwall/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from picwall.models import Picture, PictureComment, PhotoWall, PhotoInformation, WebSiteUser
-
-admin.site.register(Picture)
-admin.site.register(PictureComment)
-admin.site.register(PhotoWall)
-admin.site.register(PhotoInformation)
-admin.site.register(WebSiteUser)
