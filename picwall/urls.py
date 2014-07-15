fea0d74566eedd554e1e411204c7781de64381b2
doublehh <sserdoubleh@gamil.com>
2014-07-15 13:50:44 +0800

    tp

--- picwall/urls.py
+++ picwall/urls.py
@@ -2,4 +2,12 @@ from django.conf.urls import patterns, include, url
 from picwall import views
 
 urlpatterns = patterns('',
-		)
+	url(r'^$', views.index, name='index'),
+
+	# user maintain
+	url(r'^login/$', views.log_in, name='login'),
+	url(r'^logout/$', views.log_out, name='logout'),
+	url(r'^register/$', views.register, name='register'),
+	url(r'^user/info/$', views.get_user_info, name='get user info'),
+	url(r'^user/pics/$', views.get_user_pics, name='get user pics'),
+)
